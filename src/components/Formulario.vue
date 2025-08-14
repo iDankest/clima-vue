e1f41664efe459787a9304f6b988f612216c23c5
Kilian <dankestmirror@hotmail.es>
2025-08-14 12:30:02 +0100

    componet form vue init

---
+++ src/components/Formulario.vue
@@ -0,0 +1,35 @@
+<script setup>
+const paises = [
+    {codigo: AR, nombre: 'Argentina'},
+    {codigo: BR, nombre: 'Brasil'},
+    {codigo: CL, nombre: 'Chile'},
+    {codigo: CO, nombre: 'Colombia'},
+    {codigo: EC, nombre: 'Ecuador'},
+    {codigo: PY, nombre: 'Paraguay'},
+    {codigo: PE, nombre: 'Perú'},
+    {codigo: VE, nombre: 'Venezuela'},
+]
+</script>
+
+<template>
+    <form action="" class="formulario">
+        <div>
+            <label for="ciudad">Ciudad:</label>
+            <input type="text" id="ciudad" placeholder="Escribe el nombre de la ciudad">
+        </div>
+        <div>
+            <label for="pais">País:</label>
+            <select name="pais" id="pais">
+                <option value="">Selecciona un país</option>
+                <option v-for="pais in paises" :key="pais.codigo" :value="pais.codigo">{{ pais.nombre }}</option>
+            </select>
+        </div>
+        <div>
+            <label for="ciudad">Ciudad:</label>
+            <input type="text" id="ciudad" placeholder="Escribe el nombre de la ciudad">
+        </div>
+
+    </form>
+</template>
+
+
